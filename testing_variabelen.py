86c0c7e6eefa9f9ef971b8f0a4df5b9fc491986f
TJekel <131353379+TJekel@users.noreply.github.com>
2023-09-02 13:59:58 +0000

    last push

--- testing_variabelen.py
+++
@@ -1,5 +0,0 @@
-"""inhoud bekijken van de variabelen"""
-
-from openfisca_core import variables
-
-print(variables.__doc__) 
---
+++ testing_vars.py
@@ -0,0 +1,8 @@
+"""inhoud bekijken van de variabelen"""
+from openfisca_country_template.entities import Person
+
+print("Testing")
+
+
+
+tobias = Person()
